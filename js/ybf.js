a44c5586139854b644b6e31b6c89d86c782b01d9
Dornyi Adam Robert <94841029+DornyiAdam244@users.noreply.github.com>
2024-02-20 14:07:22 +0100

    asd

--- js/ybf.js
+++ js/ybf.js
@@ -19,16 +19,58 @@
 
 function validateForm(event) {
     var form = document.querySelector('.needs-validation');
+    var select = document.querySelector("select").value
 
     if (form.checkValidity() === false) {
         event.preventDefault();
         event.stopPropagation();
-    } else {
-        alert('Számoljuk az etető árát!');
     }
 
     form.classList.add('was-validated');
 }
-document.getElementById("ticketSelect").addEventListener("change", function () {
-    calculateTicketPrice(this);
-});
+
+
+function calculatePrice() {
+    var select = document.querySelector("select").value;
+    var radio_s = document.querySelector("#validationFormCheck2:checked");
+    var radio_m = document.querySelector("#validationFormCheck3:checked");
+    var radio_l = document.querySelector("#validationFormCheck4:checked");
+    var calculated = document.querySelector(".calculated h3");
+    var selectedValue = parseInt(select);
+    var choosenwood;
+    if (selectedValue !== "" && !isNaN(selectedValue)) {
+        choosenwood = selectedValue * 1500;
+    } else {
+        choosenwood = 0;
+    }
+    var size;
+    if (radio_s) {
+        size = 1.6;
+    }
+    else if (radio_m) {
+        size = 2.3;
+    }
+    else if (radio_l) {
+        size = 2.7;
+    }
+    else {
+        size = 0;
+    }
+
+    var price = (Math.floor(choosenwood * size));
+    var element = document.querySelector(".calculated");
+    if (0 < price) {
+        element.style.display = "flex";
+        calculated.innerHTML = "Kalkulált összeg: " + price + "Ft";
+    }
+    
+    if (size == 0) {
+        alert("Kérlek, válaszd ki a madáretető méretét!");
+    }
+    else if (choosenwood == 0) {
+        alert("Kérlek, válaszd ki a faanyag típusát!");
+    }
+    validateForm(event);
+
+}
+
