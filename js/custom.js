d32dd45b9bd8cf17b47e34b2a1f9ad01f901a729
Dornyi Adam Robert <94841029+DornyiAdam244@users.noreply.github.com>
2024-02-25 22:03:21 +0100

    upd

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,11 @@
 (() => {
     'use strict'
   
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
-      Array.from(forms).forEach(form => {
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
           event.preventDefault()
