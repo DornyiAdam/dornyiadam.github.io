7e2c846ddee4f1ccb59b2228d61cfef85d460bd0
Dornyi Adam Robert <94841029+DornyiAdam244@users.noreply.github.com>
2024-03-03 00:57:55 +0100

    asd

--- js/ybf.js
+++ js/ybf.js
@@ -1,75 +1,248 @@
+
+
+
+
+var counter = parseInt(localStorage.getItem("cartCounter")) || 0;
+var price = parseInt(localStorage.getItem("priceCounter")) || 0;
+function addToCart(product) {
+  counter++;
+  price += 0
+  localStorage.setItem("cartCounter", counter);
+  var checkOut = document.querySelector(".cartItemCount");
+  var mobileCheckOut = document.querySelector(".mobile-cart-items")
+  checkOut.innerHTML = " " + counter;
+  mobileCheckOut.innerHTML = " " + counter;
+  checkOut.style.padding = "4px 8px 8px 8px";
+  console.log(counter)
+  localStorage.setItem("priceCounter", price);
+  
+}
+function addToCart375400(product) {
+  addToCart(product)
+  price += 375400
+  localStorage.setItem("priceCounter", price);
+}
+function addToCart7500(product) {
+  addToCart(product)
+  price += 7500
+  localStorage.setItem("priceCounter", price);
+}
+function addToCart5000(product) {
+  addToCart(product)
+  price += 5000
+  localStorage.setItem("priceCounter", price);
+}
+function addToCart2000(product) {
+  addToCart(product)
+  price += 2000
+  localStorage.setItem("priceCounter", price);
+}
+function addToCart10000(product) {
+  addToCart()
+  price += 10000;
+  localStorage.setItem("priceCounter", price);
+}
+function clearCart() {
+  var checkOut = document.querySelector(".cartItemCount");
+  var mobileCheckOut = document.querySelector(".mobile-cart-items")
+  checkOut.innerHTML = 0
+  mobileCheckOut.innerHTML = 0
+  counter = 0;
+  price = 0;
+  localStorage.setItem("cartCounter", counter);
+  localStorage.setItem("priceCounter", price);
+  calculateCheckOut()
+  
+}
+window.onload = function reload() {
+  var checkOut = document.querySelector(".cartItemCount");
+  var mobileCheckOut = document.querySelector(".mobile-cart-items");
+  checkOut.innerHTML = " " + counter;
+  mobileCheckOut.innerHTML = " " + counter;
+  checkOut.style.padding = "4px 8px 8px 8px";
+  
+  calculateCheckOut()
+};
+
+function calculateCheckOut() {
+  sub = document.querySelector("#subtotal");
+  net = document.querySelector("#netTotal");
+  total = document.querySelector("#total");
+  vat = document.querySelector("#vat");
+  total.innerHTML = price + "Ft"
+  net.innerHTML = price*0.73 + "Ft"
+  vat.innerHTML = Math.floor(price*0.27) + "Ft"
+
+}
+
+
+
+
+var productsData = [
+  {
+    name: "Lorem model 2500",
+    image: "images/birdfeeder1.webp",
+    discount: 21,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 9600,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2800 SUPER",
+    image: "images/birdfeeder2.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2700 SUPER",
+    image: "images/birdfeeder3.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2600",
+    image: "images/birdfeeder4.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2500",
+    image: "images/birdfeeder5.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2400",
+    image: "images/birdfeeder6.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2100",
+    image: "images/birdfeeder7.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2400 SUPER",
+    image: "images/birdfeeder8.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2300 SUPER",
+    image: "images/birdfeeder9.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2000",
+    image: "images/birdfeeder10.webp",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2000 SUPER",
+    image: "images/birdfeeder6.jpg",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  },
+  {
+    name: "Lorem model 2200 SUPER",
+    image: "images/birdfeeder17.jpg",
+    discount: 25,
+    capacity: 3,
+    size: "S (5''x4'')",
+    oldPrice: 10000,
+    newPrice: 7500
+  }
+];
 (() => {
-    'use strict'
+  'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
 
-            form.classList.add('was-validated')
-        }, false)
-    })
+      form.classList.add('was-validated')
+    }, false)
+  })
 })()
 
 function validateForm(event) {
-    var form = document.querySelector('.needs-validation');
-    var select = document.querySelector("select").value
+  var form = document.querySelector('.needs-validation');
+  var select = document.querySelector("select").value
 
-    if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-    }
+  if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
 
-    form.classList.add('was-validated');
+  form.classList.add('was-validated');
 }
-var counter = 0
-function fill() {
-  counter++;
-  final = document.querySelector(".final");
-  final2 = document.querySelector(".final2");
-  final2p = document.querySelector(".final-container-mobile p")
-  final.innerHTML = counter;
-  final2.innerHTML = counter;
-  if (10 > counter) {
-    final.style.left = "25px";
-  }
-  else {
-    final.style.left = "18px"
-    final2.style.left = "18px";
-  }
-  if (counter<10) {
-    final2.style.left = "18px"
-  }
-  else {
-    final2.style.left = "14px"
-  }
-}
+
+
+
+
 function calculatePrice() {
-    var calculated = document.querySelector(".calculated h3");
-    var choosenwood = parseFloat(document.querySelector("select").value) || 0;
-    var size = document.querySelector("#validationFormCheck2:checked") ? 1.5 : (document.querySelector("#validationFormCheck3:checked") ? 2 : (document.querySelector("#validationFormCheck4:checked") ? 3 : 0));
-    var price = (Math.floor(choosenwood * size));
-    var element = document.querySelector(".calculated");
-    if (size == 0) {
-        alert("Kérlek, válaszd ki a madáretető méretét!");
-    }
-    else if (choosenwood == 0) {
-        alert("Kérlek, válaszd ki a faanyag típusát!");
-    }
-    else {
-        element.style.display = "flex";
-        calculated.innerHTML = "Kalkulált összeg: " + price + "Ft";
-    }
-    validateForm(event);
-    const myModal = new bootstrap.Modal(document.getElementById('myModal'))
-    //myModal.show();
+  var calculated = document.querySelector(".calculated h3");
+  var choosenwood = parseFloat(document.querySelector("select").value) || 0;
+  var size = document.querySelector("#validationFormCheck2:checked") ? 1.5 : (document.querySelector("#validationFormCheck3:checked") ? 2 : (document.querySelector("#validationFormCheck4:checked") ? 3 : 0));
+  var price = (Math.floor(choosenwood * size));
+  var element = document.querySelector(".calculated");
+  if (size == 0) {
+    alert("Kérlek, válaszd ki a madáretető méretét!");
+  }
+  else if (choosenwood == 0) {
+    alert("Kérlek, válaszd ki a faanyag típusát!");
+  }
+  else {
+    element.style.display = "flex";
+    calculated.innerHTML = "Kalkulált összeg: " + price + "Ft";
+  }
+  validateForm(event);
+  const myModal = new bootstrap.Modal(document.getElementById('myModal'))
+  //myModal.show();
 }
+
 /*Ezt majd később hozzáadom, hogy jó legye a modal: 
 <div class="modal-dialog modal-dialog-centered">
 <div class="modal-content">
